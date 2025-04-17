544233ef609735c81cf88f1642f1377869d2b8f7
vijayaraja <vijayaraja.s@platform3solutions.com>
2025-04-17 15:57:05 +0530

    Initialize a Spring Boot project for Resource Monitor
    Set up a basic Spring Boot application with Maven configuration. Included dependencies for Actuator, Web, DevTools, Micrometer, and testing. Added a main application class and a corresponding test class to verify context loading.

---
+++ src/main/java/com/p3/resourcemonitor/ResourceMonitorApplication.java
@@ -0,0 +1,13 @@
+package com.p3.resourcemonitor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ResourceMonitorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ResourceMonitorApplication.class, args);
+    }
+
+}
